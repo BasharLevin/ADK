8b1ad209cfb50161e8099fb22f07fc296ef0b318
basharlevin <basharaboalneaj@outlook.com>
2024-09-12 19:38:36 +0200

    hasfunction

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
 
-int main()
-{
+int hash_value_(std::string &wprefix) {
+    int n = wprefix.length();
+    int value = 0;
 
-    std::cout << "Hello, World!" << std::endl;
+    if (n >= 3) {
+        value = (static_cast<unsigned char>(wprefix[0]) * 31 * 31 +
+                 static_cast<unsigned char>(wprefix[1]) * 31 +
+                 static_cast<unsigned char>(wprefix[2]));
+    }
+    else if (n == 2) {
+        value = (static_cast<unsigned char>(wprefix[0]) * 31 +
+                 static_cast<unsigned char>(wprefix[1]));
+    }
+    else if (n == 1) {
+        value = static_cast<unsigned char>(wprefix[0]);
+    }
+    return value % 27000;
+}
+
+int main() {
+    std::string word;
+
+    std::cout << "Enter a word : ";
+    std::cin >> word;
+
+    int hash_value = hash_value_(word);
+
+    std::cout << "The hash value of '" << word << "' is: " << hash_value << std::endl;
+
     return 0;
 }
