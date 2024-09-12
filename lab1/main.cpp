3c16808baf859d828e87fe93a441a1d3f3cd1103
basharlevin <basharaboalneaj@outlook.com>
2024-09-12 19:49:17 +0200

    search funciton

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,5 +1,8 @@
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 int hash_value_(std::string &wprefix) {
     int n = wprefix.length();
@@ -18,6 +21,26 @@ int hash_value_(std::string &wprefix) {
         value = static_cast<unsigned char>(wprefix[0]);
     }
     return value % 27000;
+}
+
+std::unordered_map<int, std::vector<long>> store_hashes(const std::string& file_name) {
+    std::unordered_map<int,std::vector<long>> hash_map;
+    std::ifstream file(file_name);
+
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file " << file_name << std::endl;
+        return hash_map;
+    }
+    std::string word;
+    long line_number = 0;
+
+    while (file >> word) {
+        int hash_value = hash_value_(word);
+        hash_map[hash_value].push_back(line_number);
+        line_number += word.length() + 1;
+        file.close();
+        return hash_map;
+    }
 }
 
 int main() {
